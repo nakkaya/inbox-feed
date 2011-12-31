df475c404a1a3d70ab5820cf1ac44cdaa61199c7
Nurullah Akkaya <nurullah@nakkaya.com>
2011-12-31 11:58:58 +0200

    add test

--- test/inbox_feed/test/core.clj
+++ test/inbox_feed/test/core.clj
@@ -2,6 +2,25 @@
   (:use [inbox-feed.core])
   (:use [clojure.test]))
 
+(setup-logging)
+
+(defn- create-dummy-fs []
+  (spit (java.io.File. "config.clj")
+	(with-out-str
+          (prn [:smtp-creds {:to "nurullah@nakkaya.com"}
+                :feed-list [["http://news.ycombinator.com/rss" 15 "3cdbbd1e-5559-45d5-8dd6-d60408301580"]]]))))
+
+(defn- delete-dummy-fs []
+  (.delete (java.io.File. "config.clj"))
+  (.delete (java.io.File. "feeds.data")))
+
+(defn dummy-fs-fixture [f]
+  (create-dummy-fs)
+  (f)
+  (delete-dummy-fs))
+
+(use-fixtures :once dummy-fs-fixture)
+
 (deftest feed-test
   (let [f1 [{:id :id1 :title :title1}
             {:id :id2 :title :title2}
@@ -19,3 +38,11 @@
   (is (= [1 2 3 4 5 6] (fixed-size-seq [1 2 3 4 5 6] [])))
   (is (= (concat [2 1] (range 0 498)) (fixed-size-seq (range 0 500) [1 2]))))
 
+(deftest no-send-test
+  (let [config (prepare-config "config.clj")
+        state (prepare-state "feeds.data")]
+    (discard-feeds state config)
+    (let [url (-> config :feed-list first first)
+          old-state (state url)
+          curr-state (feed-state (:entries (parse url)))]
+      (is (= 0 (count (diff-feed-entries curr-state old-state)))))))
