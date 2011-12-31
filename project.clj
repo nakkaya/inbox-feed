0f4fedd4b537c3934ad0c8503602c4a98bb76044
Nurullah Akkaya <nurullah@nakkaya.com>
2011-12-31 13:49:47 +0200

    bump hiccup version

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject inbox-feed "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.draines/postal "1.7-SNAPSHOT"]
                  [org.clojars.nakkaya/rome "1.0"]
                  [org.clojars.nakkaya/jdom "1.1.2"]
                  [org.clojure/tools.cli "0.1.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.clojars.paraseba/hiccup "0.2.3"]]
+                 [hiccup "0.3.8"]]
   :jvm-opts ["-Dfile.encoding=UTF-8"]
   :main inbox-feed.core
   :jar-name "inbox-feed.jar"
