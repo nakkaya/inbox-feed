558a799259840ee1dc61d45fb3fe17762d0df4ed
Nurullah Akkaya <nurullah@nakkaya.com>
2012-05-15 17:21:19 +0300

    update postal

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject inbox-feed "1.0.0-SNAPSHOT"
   :description "RSS to Email"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.draines/postal "1.7-SNAPSHOT"]
+                 [org.clojars.doo/postal "1.8-SNAPSHOT"]
                  [org.clojars.nakkaya/rome "1.0"]
                  [org.clojars.nakkaya/jdom "1.1.2"]
                  [org.clojure/tools.cli "0.2.1"]
